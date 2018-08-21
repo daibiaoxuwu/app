226cd2f10b76ebc971ad519d7aa9367c98c0bbce
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2018-08-21 16:21:59 +0800

    add drawer and drawerbutton

--- src/main/java/com/example/d/test/feature/MainActivity.java
+++ src/main/java/com/example/d/test/feature/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.d.test.feature;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         return newsItemList;
     }
 
+    private DrawerLayout drawerLayout;
+
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
         getMenuInflater().inflate(R.menu.toolbar, menu);
@@ -44,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
         } else if (i == R.id.menu_settings) {
             Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+        } else if (i == android.R.id.home) {
+            Toast.makeText(this, "drawer", Toast.LENGTH_SHORT).show();
+            drawerLayout.openDrawer(GravityCompat.START);
         }
         return true;
     }
@@ -64,6 +73,13 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         RecycledNewsAdapter adapter=new RecycledNewsAdapter(MainActivity.this, newsItemList);
         recyclerView.setAdapter(adapter);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        ActionBar actionBar=getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        }
     }
 
     private void initNews(){
