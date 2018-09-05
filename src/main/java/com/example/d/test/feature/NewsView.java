74cc0e542a28700aa607e6f885f0ced268c82a31
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2018-09-05 10:07:08 +0800

    Webview. add phone UI, but with selection.!but without database

--- src/main/java/com/example/d/test/feature/NewsView.java
+++ src/main/java/com/example/d/test/feature/NewsView.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -22,8 +24,22 @@ public class NewsView extends AppCompatActivity {
 //        Log.d(TAG, "onCreate: " + url);
 
         WebView webView=findViewById(R.id.web_view);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webView.getSettings();
+        settings.setUserAgentString("app/XXX");//添加UA,  “app/XXX”：是与h5商量好的标识，h5确认UA为app/XXX就认为该请求的终端为App
+        settings.setJavaScriptEnabled(true);
+
+        //设置参数
+        settings.setBuiltInZoomControls(true);
+        settings.setAppCacheEnabled(true);// 设置缓存
+
+        webView.setWebChromeClient(new WebChromeClient());
+//        webView.setWebViewClient(new WebViewClient());
+
+//        webView.loadUrl(loadurl);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webview.getSettings().setUserAgentString(ua+"; 自定义标记");
+//        webView.setWebViewClient(new WebViewClient());
 //        Toast.makeText(NewsView.this,url,Toast.LENGTH_SHORT).show();
         webView.loadUrl(url);
     }
